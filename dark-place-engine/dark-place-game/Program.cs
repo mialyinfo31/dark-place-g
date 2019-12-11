65fab36596eaf6d8d5b6e594e05da8e8f288bfa4
Mia <mialy.info@gmail.com>
2019-12-11 09:47:53 +0100

    First commit

---
+++ dark-place-engine/dark-place-game/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dark_place_game
+{
+    class Program
+    {
+        // private GameWorld gameWorld;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lancement du jeu Dark Place ...");
+            throw new Exception("Le jeu ne semble pas encore codé !");
+        }
+    }
+
+}
+
---
+++ dark-place-game/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dark_place_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
